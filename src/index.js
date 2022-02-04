ef0deb202bbec598890d8a4ba209baad21c2fec3
VVP <pechathebest@yandex.ru>
2022-02-04 23:35:56 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -38,8 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split("");
+
+    let size = 2;
+    let subarray = [];
+    for (let i = 0; i < arr.length/size; i++){
+    subarray[i] = (arr.slice((i*size), (i*size) + size)).join("");
+    }
+  console.log(subarray)
+    let subarray2 = subarray.map(function(item) {
+        if (item == "10") {
+            item = ".";
+            return item;
+        } else if (item == "11") {
+            item = "-";
+            return item;
+        } else if (item == "**") {
+             item = "/";
+            return item;
+        }
+    });
+console.log(subarray2);
+    size = 5;
+    let subarray3 = [];
+    for (let i = 0; i < subarray2.length/size; i++){
+    subarray3[i] = (subarray2.slice((i*size), (i*size) + size)).join("");
+    }
+console.log(subarray3);
+    let subarray4 = subarray3.map(function(item) {
+        if (item == "/////") {
+            item = " ";
+            return item;
+        } else {
+            item = MORSE_TABLE[item];
+            return item;
+        }
+    });
+    return subarray4.join("");
 }
+
 
 module.exports = {
     decode
